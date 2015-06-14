5b3a27d982c1746b40b126878fe5551b10cc0c11
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2015-06-14 22:29:47 +0530

    Optimize learning rate for better error rate

--- mlp_synthdata.py
+++ mlp_synthdata.py
@@ -9,26 +9,18 @@ from deepy.layers import Dense, Softmax
 from deepy.trainers import MomentumTrainer, LearningRateAnnealer
 from SynthDataset import SynthDataset
 
-model_path = os.path.join(os.path.dirname(__file__), "models", "model_1000_op.gz")
+model_path = os.path.join(os.path.dirname(__file__), "models", "model_10000_op.gz")
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    ap = ArgumentParser()
-    ap.add_argument("--load", default="", help="pre-trained model path")
-    ap.add_argument("--finetune", action="store_true")
-    args = ap.parse_args()
-    
     model = NeuralClassifier(input_dim=32*32)
-    model.stack(Dense(100, 'tanh'),
+    model.stack(Dense(400, 'tanh'),
                  Dense(100, 'tanh'),
                  Dense(3, 'linear'),
                  Softmax())
-    
-    if args.load:
-        model.load_params("synth_1000.gz")
-        print "loading " + args.load
 
-    trainer = MomentumTrainer(model, {"weight_l2": 0.001})
+    #trainer = MomentumTrainer(model, {"weight_l2": 0.01})
+    trainer = MomentumTrainer(model, {"learning_rate": LearningRateAnnealer.learning_rate(0.001)})
+    
 
     annealer = LearningRateAnnealer(trainer)
 
